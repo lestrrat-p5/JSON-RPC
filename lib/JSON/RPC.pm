d6702da3d8142f4ccbc31f4cf11a1acd78da7e39
lestrrat <lestrrat+github@gmail.com>
2011-11-16 11:59:18 +0900

    more docs

--- lib/JSON/RPC.pm
+++ lib/JSON/RPC.pm
@@ -45,7 +45,36 @@ Router::Simple. For example, if you want to map method "foo" to a "MyApp::JSONRP
         };
     };
 
-The "+" prefix in the handler classname denotes that it is already a fully qualified classname. Without the prefix, the value of "prefix" in the dispatcher object will be used to qualify the classname.
+The "+" prefix in the handler classname denotes that it is already a fully qualified classname. Without the prefix, the value of "prefix" in the dispatcher object will be used to qualify the classname. If you specify it in your Dispatcher instance, you may omit the prefix part to save you some typing:
+
+    use JSON::RPC::Dispatcher;
+    use Router::Simple::Declare;
+
+    my $router = router {
+        connect "foo" => {
+            handler => "Foo",
+            action  => "process",
+        };
+        connect "bar" => {
+            handler => "Bar",
+            action => "process"
+        }
+    };
+    my $dispatcher = JSON::RPC::Dispatcher->new(
+        prefix => "MyApp::JSONRPC::Handler",
+        router => $router,
+    );
+
+    # The above will roughly translate to the following:
+    #
+    # for method "foo"
+    #    my $handler = MyApp::JSONRPC::Handler::Foo->new;
+    #    $handler->process( ... );
+    #
+    # for method "bar"
+    #    my $handler = MyApp::JSONRPC::Handler::Bar->new;
+    #    $handler->process( ... );
+
 
 The implementors are called handlers. Handlers are simple objects, and will be instantiated automatically for you. Their return values are converted to JSON objects automatically.
 
@@ -61,6 +90,37 @@ You may also choose to pass objects in the handler argument to connect in  your 
             action  => "handle_foo"
         };
     };
+
+Your handlers are responsible for returning some sort of reference structure
+that can be properly encoded via JSON/JSON::XS.
+
+The handler methods will receive the following parameters:
+
+    sub your_handler_method {
+        my ($self, $params, $procedure, @extra_args) = @_;
+
+        return $some_structure;
+    }
+
+In most cases you will only need the parameters. The exact format of the $params is dependend on the caller -- you will be passed whatever JSON structure that caller used to call your handler.
+
+$procedure is an instance of JSON::RPC::Procedure. Use it if you need to figure out more about the procedure.
+
+@extra_args is optional, and will be filled with whatever extra arguments you passed to handle_psgi(). For example, 
+
+    # app.psgi
+    sub {
+        $dispatcher->handle_psgi($env, "arg1", "arg2", "arg3");
+    }
+
+will cause your handlers to receive the following arguments:
+
+    sub your_handler_method {
+        my ($self, $params, $procedure, $arg1, $arg2, $arg3) = @_;
+
+    }
+
+This is convenient if you have application-specific data that needs to be passed to your handlers.
 
 =head1 EMBED IT IN YOUR WEBAPP
 
@@ -95,5 +155,17 @@ Yoshimitsu Torii
 =head1 AUTHOR EMERITUS
 
 Makamaka Hannyaharamitu, E<lt>makamaka[at]cpan.orgE<gt> - JSON::RPC modules up to 0.96
+
+=head1 COPYRIGHT AND LICENSE
+
+The JSON::RPC module is
+
+Copyright (C) 2011 by Daisuke Maki
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+See JSON::RPC::Legacy for copyrights and license for previous versions.
 
 =cut
